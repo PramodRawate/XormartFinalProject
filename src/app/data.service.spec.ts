7645f7a49d66c3cd7fc0748303d0d440af274f43
PramodRawate <rawatepramosmd@gmail.com>
2018-11-12 17:51:40 +0530

    Woking on DataService's Validate method Spec

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,11 +1,22 @@
 import { TestBed, inject, fakeAsync } from '@angular/core/testing';
 import { DataService } from './data.service';
-
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage'
+import { CookieService } from '../../node_modules/ngx-cookie-service';
 import { HttpModule,XHRBackend,ResponseOptions,Response,RequestMethod } from '@angular/http';
 import { MockBackend,MockConnection } from '@angular/http/testing';
 
 let dataservice:DataService;
 const mockUsers = [
+                        {  
+                            address: "B3-302, BlueRidge, Hinjewadi Pune",
+                            contact: "075524668571",
+                            email: "promodR@gmail.com",
+                            firstname: "Pramod",
+                            lastname: "Rawate",
+                            password: "5136sad",
+                            username: "promodak",
+                            _id: "5b750c924748588debb5d649"
+                        },
                         {   
                             address: "Mannat,Bandra",
                             contact: "075524668571",
@@ -14,16 +25,6 @@ const mockUsers = [
                             lastname: "khan",
                             password: "bazzigar",
                             username: "iamsrk",
-                            _id: "5b750c924748588debb5d649"
-                        },
-                        {  
-                            address: "B3-302, BlueRidge, Hinjewadi Pune",
-                            contact: "075524668571",
-                            email: "promodR@gmail.com",
-                            firstname: "Pramod",
-                            lastname: "Rawate",
-                            password: "5136sad",
-                            username: "promodk",
                             _id: "5b750c924748588debb5d649"
                         }
                 ]
@@ -37,32 +38,49 @@ describe('DataService', () => {
                 provide:XHRBackend,
                 useClass:MockBackend
             },
-            DataService
+            MockBackend,
+            DataService,
+            CookieService,
+            LocalStorageService, 
+            SessionStorageService 
         ]
-        })
+        });
     });
 
 
-    
+    it('should validate user and get MongoDb response', fakeAsync(
+        inject([
+            XHRBackend,
+            DataService
+        ],(mockBackend:XHRBackend,dataservice:DataService) => {
+          
+            const expectedUrl = '/api/validate';
+            TestBed.get(MockBackend).connections.subscribe(
+                (connection:MockConnection) => {
+                    expect(connection.request.method).toBe(RequestMethod.Get);
+                    expect(connection.request.url).toBe(expectedUrl);
 
-    beforeEach(inject([DataService],(ds:DataService) => {
-        dataservice=ds;
-        spyOn(DataService.prototype,'getSessionStorage').and.returnValue('promodk');
-    }))
+                    connection.mockRespond(new Response(
+                        new ResponseOptions({body:mockUsers})
+                    ));
+                });
 
-        it('should get session storage and return user name',() => {
-        // expect(dataservice.getSessionStorage()).toEqual('promodk');
-            expect(dataservice.getSessionStorage()).toEqual('promodk');
+                dataservice.validateUser({'username':'promodk','password':'5136sad'})
+                    .subscribe(res => {
+                        expect(res).toEqual(mockUsers);
+                    })
         })
+    ))
 });
 
-// it('should validate user and get MongoDb response', fakeAsync(
-//     inject([
-//         XHRBackend,
-//         DataService
-//     ],(mockBackend:XHRBackend,dataservice:DataService) => {
-      
-//         const expectedUrl = '/api/validate';
-//         mockBackend
-//     })
-// ))
+
+    //  *********** Add this after beforeEach **********************
+    // beforeEach(inject([DataService],(ds:DataService) => {
+    //     dataservice=ds;
+    //     spyOn(DataService.prototype,'getSessionStorage').and.returnValue('promodk');
+    // }))
+
+    //     it('should get session storage and return user name',() => {
+    //     // expect(dataservice.getSessionStorage()).toEqual('promodk');
+    //         expect(dataservice.getSessionStorage()).toEqual('promodk');
+    //     })
